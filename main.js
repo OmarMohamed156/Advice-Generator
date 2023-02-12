b1a785771907c076f75575424943741335845124
OmarMohamed156 <om658036@gmail.com>
2023-02-12 23:29:16 +0200

    project done

--- main.js
+++ main.js
@@ -2,12 +2,11 @@ function getRandomAdvice() {
   fetch("https://api.adviceslip.com/advice")
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
       document.getElementById("title").innerHTML = `ADVICE # ${data?.slip?.id}`;
       document.getElementById("advice").innerHTML = `“ ${data?.slip?.advice} ”`;
     })
     .catch((err) => {
-      console.log(err);
+      document.getElementById('card').innerHTML(err)
     });
 }
 
